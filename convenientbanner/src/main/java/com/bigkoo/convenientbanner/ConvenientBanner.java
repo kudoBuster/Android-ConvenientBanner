4002b1d237d3a5a11f476029dd3816b901c1aa5b
buster <kudo_buster@163.com>
2017-06-16 15:06:59 +0800

    添加指示器位置margin

--- convenientbanner/src/main/java/com/bigkoo/convenientbanner/ConvenientBanner.java
+++ convenientbanner/src/main/java/com/bigkoo/convenientbanner/ConvenientBanner.java
@@ -179,11 +179,12 @@ public class ConvenientBanner<T> extends LinearLayout {
      * @param align  三个方向：居左 （RelativeLayout.ALIGN_PARENT_LEFT），居中 （RelativeLayout.CENTER_HORIZONTAL），居右 （RelativeLayout.ALIGN_PARENT_RIGHT）
      * @return
      */
-    public ConvenientBanner setPageIndicatorAlign(PageIndicatorAlign align) {
+    public ConvenientBanner setPageIndicatorAlign(PageIndicatorAlign align, int left, int top, int right, int bottom) {
         RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) loPageTurningPoint.getLayoutParams();
         layoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT, align == PageIndicatorAlign.ALIGN_PARENT_LEFT ? RelativeLayout.TRUE : 0);
         layoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, align == PageIndicatorAlign.ALIGN_PARENT_RIGHT ? RelativeLayout.TRUE : 0);
         layoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL, align == PageIndicatorAlign.CENTER_HORIZONTAL ? RelativeLayout.TRUE : 0);
+        layoutParams.setMargins(left, top, right, bottom);
         loPageTurningPoint.setLayoutParams(layoutParams);
         return this;
     }
